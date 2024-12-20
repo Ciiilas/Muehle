a432196f9a6ef8732bb56f646bb948545247102f
lememin <164020341+lememin@users.noreply.github.com>
2024-12-20 10:33:45 +0100

    added some brakets for debuging checkForMuehle

--- src/main/scala/de/htwg/se/muehle/model/mechanic/ComplexEvaltuateStrategy.scala
+++ src/main/scala/de/htwg/se/muehle/model/mechanic/ComplexEvaltuateStrategy.scala
@@ -4,15 +4,15 @@ import de.htwg.se.muehle.model.gamefield.{Gamefield, Stone}
 
 class ComplexEvaltuateStrategy extends EvaluateStrategy {
   override def checkForMuehle(field: Gamefield, newRing: Int, newPosOnRing: Int, stone: Stone): Boolean = {
-      //check Muehle from Ring Center
+      //check Muehle an this Ring Center
       if (newPosOnRing % 2 == 1) {
         //checking On Ring
         //newPosOnRing == (oldPosOnRing + 1) % 8 || newPosOnRing == (oldPosOnRing - 1 + 8) % 8
-        if (field.muehleMatrix(newRing)((newPosOnRing - 1 + 8) % 8) == stone && field.muehleMatrix(newRing)((newPosOnRing + 1) % 8) == stone) {
+        if ((field.muehleMatrix(newRing)((newPosOnRing - 1 + 8) % 8) == stone) && (field.muehleMatrix(newRing)((newPosOnRing + 1) % 8) == stone)) {
           return true
         }
         //checking other Rings
-        if (field.muehleMatrix((newRing - 1 + 3) % 3)(newPosOnRing) == stone && field.muehleMatrix((newRing + 1) % 3)(newPosOnRing) == stone) {
+        if ((field.muehleMatrix((newRing - 1 + 3) % 3)(newPosOnRing) == stone) && (field.muehleMatrix((newRing + 1) % 3)(newPosOnRing) == stone)) {
           return true
         }
         return false
@@ -20,8 +20,8 @@ class ComplexEvaltuateStrategy extends EvaluateStrategy {
       //check Muehle from Ring Corner
       if (newPosOnRing % 2 == 0) {
         //Corner right
-        if (field.muehleMatrix(newRing)((newPosOnRing - 1 + 8) % 8) == stone && field.muehleMatrix(newRing)((newPosOnRing - 2 + 8) % 8) == stone
-          || field.muehleMatrix(newRing)((newPosOnRing + 1) % 8) == stone && field.muehleMatrix(newRing)((newPosOnRing + 2) % 8) == stone) {
+        if ((field.muehleMatrix(newRing)((newPosOnRing - 1 + 8) % 8) == stone) && (field.muehleMatrix(newRing)((newPosOnRing - 2 + 8) % 8) == stone)
+          || (field.muehleMatrix(newRing)((newPosOnRing + 1) % 8) == stone) && (field.muehleMatrix(newRing)((newPosOnRing + 2) % 8) == stone)) {
           return true
         }
       }
