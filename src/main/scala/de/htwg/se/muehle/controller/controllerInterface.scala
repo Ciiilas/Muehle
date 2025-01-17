665a95993eff526a3e9ca2981b658cc8a2dd6b06
lememin <164020341+lememin@users.noreply.github.com>
2025-01-17 17:08:50 +0100

    added methodes from controller to controllerinterface

--- src/main/scala/de/htwg/se/muehle/controller/controllerInterface.scala
+++ src/main/scala/de/htwg/se/muehle/controller/controllerInterface.scala
@@ -2,7 +2,7 @@ package de.htwg.se.muehle.controller
 
 import de.htwg.se.muehle.util.Observer
 import de.htwg.se.muehle.model.{GameStateEnum, gameInterface}
-import de.htwg.se.muehle.model.gameFieldComponent.gamefield.Stone
+import de.htwg.se.muehle.model.gameFieldComponent.gamefield.{Stone, meshComponentInterface}
 
 trait controllerInterface {
 
@@ -16,6 +16,8 @@ trait controllerInterface {
   def moveStone(fromSquareIndex: Int, fromPointIndex: Int, toSquareIndex: Int, toPointIndex: Int): Unit
   def jumpStone(fromSquareIndex: Int, fromPointIndex: Int, toSquareIndex: Int, toPointIndex: Int): Unit
   def removeStone(squareIndex: Int, pointIndex: Int): Unit
+  def setDecorator(enabled: Boolean): Unit
+  def getMesh(): meshComponentInterface
   def undo(): Unit
   def load(): Unit
   def save(): Unit
