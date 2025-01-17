935794d990fc0d31e9264457e56a6c32d6daee6a
lememin <164020341+lememin@users.noreply.github.com>
2025-01-17 17:34:24 +0100

    added more test to gamefieldspec

--- src/test/scala/de/htwg/se/muehle/model/gamefield/GamefieldSpec.scala
+++ src/test/scala/de/htwg/se/muehle/model/gamefield/GamefieldSpec.scala
@@ -86,12 +86,33 @@ class GamefieldSpec extends AnyWordSpec {
     val field = new Gamefield("EEEEEEEE", 1, 8)
     field.withEnumAt(0, 0, Stone.White) should be(new Gamefield("WEEEEEEE", 1, 8))
   }
-  "should made a bar" in {
+  "should made a bar left" in {
     val field = new Gamefield()
-    field.barSegmentLeft(2) should be("--")
+    field.barSegmentLeft(2) should be("0─────")
   }
-  "should made a bar" in {
+  "should made a bar right" in {
     val field = new Gamefield()
-    field.barSegmentRight(2) should be("--")
+    field.barSegmentRight(2) should be("─────0")
+  }
+  "should made a spacer left top" in {
+    val field = new Gamefield()
+    field.spacerLeftTop(2) should be("│ │ ")
+  }
+  "should made a spacer right top" in {
+    val field = new Gamefield()
+    field.spacerRightTop(2) should be(" │ │")
+  }
+  "should made a spacer left bottom" in {
+    val field = new Gamefield()
+    field.spacerLeftBottom(2) should be("│ │ ")
+  }
+  "should made a spacer right bottom" in {
+    val field = new Gamefield()
+    field.spacerRightBottom(2) should be(" │ │")
+  }
+
+  "should made a segemnt left bottom" in {
+    val field = new Gamefield()
+    field.barSegmentLeftBottom(2) should be("0─────")
   }
 }
