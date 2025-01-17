8e9d8f9d010063df987b64e9e30e0a29177bceb0
lememin <164020341+lememin@users.noreply.github.com>
2025-01-17 20:23:37 +0100

    outcomment mesh testing

--- src/test/scala/de/htwg/se/muehle/model/gamefield/GamefieldSpec.scala
+++ src/test/scala/de/htwg/se/muehle/model/gamefield/GamefieldSpec.scala
@@ -21,66 +21,9 @@ class GamefieldSpec extends AnyWordSpec {
       "│     │     │\n" +
       "0─────0─────0\n"
     gamefield.mesh() should be(expectedMesh)
-  }
-
-  "mesh should return correct string representation for custom gamefield" in {
-    val gamefield = new Gamefield("WBEBEWEWE", 2, 5)
-    val expectedMesh = "W─────B─────E\n" +
-      "│     │     │\n" +
-      "│ E───W───E │\n" +
-      "│ │   │   │ │\n" +
-      "│ │ E─E─E │ │\n" +
-      "│ │ │   │ │ │\n" +
-      "0─0─0   0─0─0\n" +
-      "│ │ │   │ │ │\n" +
-      "│ │ 0─0─0 │ │\n" +
-      "│ │   │   │ │\n" +
-      "│ 0───0───0 │\n" +
-      "│     │     │\n" +
-      "0─────0─────0\n"
-    gamefield.mesh() should be(expectedMesh)
-  }
-
-  "mesh should handle empty gamefield" in {
-    val gamefield = new Gamefield(0, 0, Stone.Empty)
-    gamefield.mesh() should be("")
-  }
-
-  "mesh should handle single row gamefield" in {
-    val gamefield = new Gamefield(1, 3, Stone.White)
-    val expectedMesh = "W─────W─────W\n" +
-      "│     │     │\n" +
-      "│ 0───0───0 │\n" +
-      "│ │   │   │ │\n" +
-      "│ │ 0─0─0 │ │\n" +
-      "│ │ │   │ │ │\n" +
-      "0─0─0   0─0─0\n" +
-      "│ │ │   │ │ │\n" +
-      "│ │ 0─0─0 │ │\n" +
-      "│ │   │   │ │\n" +
-      "│ 0───0───0 │\n" +
-      "│     │     │\n" +
-      "0─────0─────0\n"
-    gamefield.mesh() should be(expectedMesh)
-  }
-
-  "mesh should handle single column gamefield" in {
-    val gamefield = new Gamefield(3, 1, Stone.Black)
-    val expectedMesh = "B─────B─────B\n" +
-      "│     │     │\n" +
-      "│ 0───0───0 │\n" +
-      "│ │   │   │ │\n" +
-      "│ │ 0─0─0 │ │\n" +
-      "│ │ │   │ │ │\n" +
-      "0─0─0   0─0─0\n" +
-      "│ │ │   │ │ │\n" +
-      "│ │ 0─0─0 │ │\n" +
-      "│ │   │   │ │\n" +
-      "│ 0───0───0 │\n" +
-      "│     │     │\n" +
-      "0─────0─────0\n"
-    gamefield.mesh() should be(expectedMesh)
   }*/
+
+ 
 
   "should change muehlematrix" in {
     val field = new Gamefield("EEEEEEEE", 1, 8)
